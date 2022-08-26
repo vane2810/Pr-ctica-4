3aa571b1380a58a2a74b068d342223b8740f01d8
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-26 12:21:52 -0600

    Cuarto proyecto

---
+++ Práctica 4.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Práctica_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable lista = new Hashtable();
+            Console.WriteLine(lista.Count);
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(i, " Dato " + i);
+            }
+
+            foreach(int key in lista.Keys)
+            {
+                Console.WriteLine("Clave: {0} === Valor: {1}", key, lista[key]);
+            }
+
+        }
+    }
+}
+ 
