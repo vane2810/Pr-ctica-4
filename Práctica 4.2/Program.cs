9e82e80c62288faef4c081928d2519f5df16f26b
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-26 11:45:04 -0600

    Segundo proyecto

---
+++ Práctica 4.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Práctica_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[3, 3] { {8, 6, 1 }, { 4, 6, 7 }, { 6, 3, 4 } };
+            foreach(int i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+               for(int j = 0; j < numeros.GetLength(1); j++)
+               {
+                    numeros[i , j] = i * j;
+                    Console.Write(numeros[i, j] + " ");
+               }
+                Console.WriteLine();
+            }
+
+            string[,] paises = { { "El Salvador" }, { "Guatemala" }, { " " } };
+             
+        }
+    }
+}
