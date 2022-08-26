a60e8d417b0dc200f9258c301d8acd58a1bee7b5
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-26 11:16:55 -0600

    Primer proyecto

--- Práctica 4/Program.cs
+++ Práctica 4/Program.cs
@@ -6,7 +6,31 @@ namespace Práctica_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array1 = new int[5];
+            array1[2] = 8;
+            array1[0] = 15;
+
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(i + " " + array1[i]);
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = i * 4;
+            }
+
+            foreach(int row in array1)
+            {
+                Console.WriteLine(row);
+            }
+
+            string[] paises = new string[5] { "El Salvador" , "México" , "Chile" , "Colombia" , "Venezuela"};
+            foreach(string pais in paises)
+            {
+                Console.WriteLine(pais);
+            }
         }
     }
 }
