0b646d53876ba91023a36ea30eed76f01c69bbdb
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-26 10:48:03 -0600

    Agregar archivos de proyecto.

---
+++ Práctica 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Práctica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
